3482205346b399c7ee5a07347e0d75f759d72e60
Alex Dewar <alexldewar@gmail.com>
2020-09-24 11:02:12 -0300

    Add license, update setup

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='cwtvla',
-    version='0.0.1',
+    version='0.1.0',
     description='ChipWhisperer Test Vector Leakage Assessment Library',
     author='Alex Dewar',
     author_email='adewar@newae.com',
-    license='GPLv3',
+    license='GPLv2+',
     packages=find_packages("cwtvla"),
     install_requires=[
         'scipy',
-        'chipwhisperer' #need this right now, but not too hard to decouple+make optional
+        #cw not really necessary, but cw convenience functions obviously require CW to be installed
+        #'chipwhisperer' #need this right now, but not too hard to decouple+make optional
     ]
 )
