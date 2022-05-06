90300a6cd148a6d0db649cb3994c15de4d2b8951
Alex Dewar <alexldewar@gmail.com>
2022-05-06 14:53:51 -0300

    Fix? cwtvla pypi

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='cwtvla',
-    version='0.1.0',
+    version='0.1.1',
     description='ChipWhisperer Test Vector Leakage Assessment Library',
     author='Alex Dewar',
     author_email='adewar@newae.com',
     license='GPLv2+',
-    packages=find_packages("cwtvla"),
+    packages=['cwtvla'],
     install_requires=[
         'scipy',
         'numpy',
+        'zarr',
+        'tqdm'
         #cw not really necessary, but cw convenience functions obviously require CW to be installed
         #'chipwhisperer' 
     ]
