35fc06510367ac8cf44a68d53bd4f00ef0be5422
Alex Dewar <alexldewar@gmail.com>
2022-07-05 10:37:04 -0300

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cwtvla',
-    version='0.1.2',
+    version='0.1.3',
     description='ChipWhisperer Test Vector Leakage Assessment Library',
     author='Alex Dewar',
     author_email='adewar@newae.com',
